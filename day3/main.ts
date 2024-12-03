03de132c9235d3671d5563402fd6ecc9df65c438
Jared Massa <mr.jaredmassa@gmail.om>
2024-12-03 16:42:35 -0500

    day 3 part 1

--- day3/main.ts
+++ day3/main.ts
@@ -1,1 +1,35 @@
-console.log('it works!');
+import * as fs from 'fs';
+import { join } from 'path';
+
+const regex = /mul\((\d+),(\d+)\)/g;
+
+type OperationCall = {
+  params: number[];
+};
+
+function parseOperations(data: string): OperationCall[] {
+  const matches = data.matchAll(regex);
+  const operations: OperationCall[] = [];
+  for (let match of matches) {
+    const op = {
+      params: [+match[1], +match[2]],
+    };
+    operations.push(op);
+  }
+  return operations;
+}
+
+function findSum(operations: OperationCall[]) {
+  let total = 0;
+  for (let operation of operations) {
+    total += operation.params[0] * operation.params[1];
+  }
+  return total;
+}
+
+const data = fs.readFileSync(join(__dirname, '/data.txt'), 'utf8');
+const operations = parseOperations(data);
+const sum = findSum(operations);
+
+console.log(`Found ${operations.length} operations`);
+console.log(`Sum: ${sum}`);
