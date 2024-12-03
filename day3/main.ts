82c610dc740016592483538a59089e39c36e54f1
Jared Massa <mr.jaredmassa@gmail.om>
2024-12-03 17:15:50 -0500

    day 3 part 2

--- day3/main.ts
+++ day3/main.ts
@@ -1,28 +1,58 @@
 import * as fs from 'fs';
 import { join } from 'path';
 
-const regex = /mul\((\d+),(\d+)\)/g;
+const regex = /do\(\)|don't\(\)|mul\((\d+),(\d+)\)/g;
 
 type OperationCall = {
-  params: number[];
+  name: 'do' | "don't" | 'mul';
+  params?: any[];
 };
 
 function parseOperations(data: string): OperationCall[] {
   const matches = data.matchAll(regex);
   const operations: OperationCall[] = [];
   for (let match of matches) {
-    const op = {
-      params: [+match[1], +match[2]],
-    };
-    operations.push(op);
+    switch (match[0]) {
+      case 'do()':
+      case "don't()":
+        console.log(match[0]);
+        operations.push({
+          name: match[0].substring(0, match[0].indexOf('(')) as 'do' | "don't",
+        });
+        break;
+      default:
+        if (match[0].startsWith('mul')) {
+          operations.push({
+            name: 'mul',
+            params: [+match[1], +match[2]],
+          });
+        }
+        break;
+    }
   }
   return operations;
 }
 
 function findSum(operations: OperationCall[]) {
+  let enabled = 1;
   let total = 0;
   for (let operation of operations) {
-    total += operation.params[0] * operation.params[1];
+    switch (operation.name) {
+      case 'do':
+        enabled = 1;
+        console.log('enabled!');
+        break;
+      case "don't":
+        enabled = 0;
+        console.log('disabled!');
+        break;
+      case 'mul':
+        if (!operation.params || operation.params.length < 2) {
+          throw new Error('Tried to process invalid operation');
+        }
+        total += operation.params[0] * operation.params[1] * enabled;
+        break;
+    }
   }
   return total;
 }
